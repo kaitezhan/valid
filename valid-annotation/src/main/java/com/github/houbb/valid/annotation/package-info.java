1e36abf492c2e029e4a772ba6df2b1e731b943e3
binbin.hou <1060732496@qq.com>
2019-10-04 11:33:45 +0800

    release branch 0.0.1

--- valid-annotation/src/main/java/com/github/houbb/valid/annotation/package-info.java
+++ valid-annotation/src/main/java/com/github/houbb/valid/annotation/package-info.java
@@ -4,11 +4,12 @@
  */
 
 /**
- * <p> </p>
+ * <p> 自定义注解文件夹 </p>
  *
  * <pre> Created: 2019-01-07 21:36  </pre>
  * <pre> Project: valid  </pre>
  *
  * @author houbinbin
+ * @since 0.0.1
  */
 package com.github.houbb.valid.annotation;
