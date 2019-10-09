910b6d00484b3c840f9a37f25d292f2d02254bbc
houbb <1060732496@qq.com>
2019-10-10 01:55:32 +0800

    release branch 0.0.8

--- release.sh
+++ release.sh
@@ -2,9 +2,9 @@
 echo "============================= RELEASE START..."
 
 ## 版本号信息(需要手动指定)
-oldVersion="0.0.1"
-newVersion="0.0.2"
-projectName="sensitive"
+oldVersion="0.0.8"
+newVersion="0.0.9"
+projectName="valid"
 
 # release 项目版本
 ## snapshot 版本号
